08ca999b2de6195e35a043b0e287441bfdbc87f3
hitmanbui <anhtb@kaiyouit.com>
2023-04-24 16:24:23 +0700

    Test app

--- lib/main.dart
+++ lib/main.dart
@@ -74,6 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
