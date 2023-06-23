d7dfaa8f4230091f71132a21249d710b7427d5fd
hitmanbui <anhtb@kaiyouit.com>
2023-06-23 17:36:20 +0700

    Test app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MiniApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Mini App Demo',
+      title: 'Mini App Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,17 +24,16 @@ class MiniApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(
-        title: 'Flutter Mini App Demo Home Page',
-        // onCLose: () => onCLose(),
+      home: MiniHomePage(
+        onClose: () => Navigator.pop(context),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MiniHomePage extends StatefulWidget {
 
-  const MyHomePage({super.key, required this.title});
+  const MiniHomePage({super.key, required this.onClose});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,13 +43,13 @@ class MyHomePage extends StatefulWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-  final String title;
+  final Function onClose;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MiniHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MiniHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -76,11 +75,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.arrow_back_ios),
-        ),
+        title: Text("Mini App Demo Home Page"),
+        leading: const Icon(Icons.arrow_back_ios),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
